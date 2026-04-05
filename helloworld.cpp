35189e0de3bae8464c95c7a8cd48b7141a5d04d0
Zach <zachharding34@gmail.com>
2026-04-05 14:49:14 -0500

    compiler opt

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,7 +1,12 @@
 #include <iostream>
+#include <limits>
 
 int main() {
     // std::cin >> 
     std::cout << "Hello, world";
+
+    std::cin.clear(); // reset any error flags
+    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n'); // ignore any characters in the input buffer until we find an enter character
+    std::cin.get(); // get one more char from the user
     return 0;
 }
