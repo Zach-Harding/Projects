3d9b2d1ca85f05039207ed5eaa88c6ac97204cb4
Zach <zachharding34@gmail.com>
2026-04-15 17:11:42 -0500

    start of new book

---
+++ testq.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <cstdio>
+#include <cstdlib>
+
+int main() {
+    int x = 10;
+    if (x>0) {
+        std::cout << "x is greater than 0\n";
+    } else if (x<0) {
+        std::cout << "x is less than 0\n";
+    } else {
+        std::cout << "x is equal to 0\n";
+    }
+    return 0;
+
+
+}
